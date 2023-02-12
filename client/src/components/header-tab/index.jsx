835d1300595c1ea127d666760023cc50923a1cdd
KhidirovaSveta <khidirova.sveta@gmail.com>
2023-02-12 21:57:51 +0400

    updated footer, added tab and updated home page

---
+++ client/src/components/header-tab/index.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Cake1 from "../../imagas/cake_slide_1.webp";
+import Cake2 from "../../imagas/cake_slide_2.webp";
+import Cake3 from "../../imagas/cake_slide_3.webp";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Image,
+} from "@chakra-ui/react";
+import Button from "../../components/button";
+import "./index.scss";
+const HeaderTab = () => {
+  return (
+    <div>
+      <Tabs defaultIndex={1} className="headerTab">
+        <TabPanels className="tabImg">
+          <TabPanel>
+            <Image boxSize="100%" fit="cover" src={Cake1} />
+            <div className="tabText">
+              <h1 className="tabHeaderTxt">Longer day, <br/> More treat.</h1>
+              <p className="tabHeaderParag">
+                Snag some cool treats now! And we'll deliver them straight to you in temperature controlled packaging.
+              </p>
+              <Button btnName={"SHOP NOW"} />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <Image boxSize="100%" fit="cover" src={Cake2} />
+            <div className="tabText">
+              <h1 className="tabHeaderTxt">Little bliss in <br/> Every bite</h1>
+              <p className="tabHeaderParag">
+                Special fluffiness for your loved one! A thoughtful expression
+                through good food and warm experiences.
+              </p>
+              <Button btnName={"SHOP NOW"} />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <Image boxSize="100%" fit="cover" src={Cake3} />
+            <div className="tabText">
+              <h1 className="tabHeaderTxt">Can't Eat <br/> Just One!</h1>
+              <p className="tabHeaderParag">
+              Snag some cool treats now! It's sweet. It's light. Flaky and buttery from first glance to the last bite.
+              </p>
+              <Button btnName={"SHOP NOW"} />
+            </div>
+          </TabPanel>
+        </TabPanels>
+        <TabList className="tabBtn">
+          <Tab className="period"></Tab>
+          <Tab className="period"></Tab>
+          <Tab className="period"></Tab>
+        </TabList>
+      </Tabs>
+    </div>
+  );
+};
+
+export default HeaderTab;
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
-import HeaderCarousel from "../../components/header-carousel";
 import Cake from "../../imagas/cake_image_card.webp";
 import Cake2 from "../../imagas/cake_image_card2.webp";
 import cakeBox from "../../imagas/cake_box_1.avif";
@@ -14,12 +13,12 @@ import cakeCollaction3 from "../../imagas/cake_collection_3.webp";
 import cakeCollaction4 from "../../imagas/cake_collection_4.webp";
 import cakeCollaction5 from "../../imagas/cake_collection_5.webp";
 import cakeCollaction6 from "../../imagas/cake_collection_6.webp";
+import HeaderTab from "../../components/header-tab";
 
 const Home = () => {
   return (
     <div id="Home">
-      <HeaderCarousel />
-
+      <HeaderTab />
       <div className="container">
         {/* Third Section */}
 
@@ -54,34 +53,34 @@ const Home = () => {
               </Link>
             </div>
             <div className="shop-card">
-            <Link to={`/`}>
-              <img src={cakeCollaction2} alt="" width={"200px"} />
-              <h3 className="shop-card-header">Pies</h3>
-            </Link>
+              <Link to={`/`}>
+                <img src={cakeCollaction2} alt="" width={"200px"} />
+                <h3 className="shop-card-header">Pies</h3>
+              </Link>
             </div>
             <div className="shop-card">
-            <Link to={`/`}>
-              <img src={cakeCollaction3} alt="" width={"200px"} />
-              <h3 className="shop-card-header">Cookis</h3>
-            </Link>
+              <Link to={`/`}>
+                <img src={cakeCollaction3} alt="" width={"200px"} />
+                <h3 className="shop-card-header">Cookis</h3>
+              </Link>
             </div>
             <div className="shop-card">
-            <Link to={`/`}>
-              <img src={cakeCollaction4} alt="" width={"200px"} />
-              <h3 className="shop-card-header">Truffle</h3>
-            </Link>
+              <Link to={`/`}>
+                <img src={cakeCollaction4} alt="" width={"200px"} />
+                <h3 className="shop-card-header">Truffle</h3>
+              </Link>
             </div>
             <div className="shop-card">
-            <Link to={`/`}>
-              <img src={cakeCollaction5} alt="" width={"200px"} />
-              <h3 className="shop-card-header">Ice Cream</h3>
-            </Link>
+              <Link to={`/`}>
+                <img src={cakeCollaction5} alt="" width={"200px"} />
+                <h3 className="shop-card-header">Ice Cream</h3>
+              </Link>
             </div>
             <div className="shop-card">
-            <Link to={`/`}>
-              <img src={cakeCollaction6} alt="" width={"200px"} />
-              <h3 className="shop-card-header">Bundle</h3>
-            </Link>
+              <Link to={`/`}>
+                <img src={cakeCollaction6} alt="" width={"200px"} />
+                <h3 className="shop-card-header">Bundle</h3>
+              </Link>
             </div>
           </div>
         </div>
