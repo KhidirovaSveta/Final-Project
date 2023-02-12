764bbf55bcf73d1e37c81344b7d1f42c759e12c1
KhidirovaSveta <khidirova.sveta@gmail.com>
2023-02-12 12:48:08 +0400

    updated footer section

--- client/src/layouts/footer/index.jsx
+++ client/src/layouts/footer/index.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./index.scss";
 import { NavLink } from "react-router-dom";
+import {
+  FaPinterestP,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import masterCard from "../../imagas/icons8-mastercard-30.svg";
+import visaCard from "../../imagas/icons8-visa-30.svg";
+import americanExp from "../../imagas/icons8-american-express-30.svg";
+import paypal from "../../imagas/icons8-paypal-30.svg";
+import discover from "../../imagas/icons8-discover-card.svg";
 
 const Footer = () => {
   return (
@@ -13,29 +24,76 @@ const Footer = () => {
               Enter your email below to be the first to know about new
               collections and product launches.
             </p>
-            <input type="email" placeholder="Enter your email" className="footerInput"/>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="footerInput"
+            />
           </div>
 
           <div className="footer-list">
             <ul>
               <li className="serv-info">SERVICES</li>
-              <li><NavLink to={"/"}> Privacy policy </NavLink></li>
-              <li> <NavLink to={"/"}> Refund policy </NavLink></li>
-              <li> <NavLink to={"/"}> Shipping & Return  </NavLink></li>
-              <li> <NavLink to={"/"}> Term & conditions  </NavLink></li>
+              <li>
+                <NavLink to={"/"}> Privacy policy </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Refund policy </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Shipping & Return </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Term & conditions </NavLink>
+              </li>
             </ul>
 
             <ul>
               <li className="serv-info">INFORMATION</li>
-              <li><NavLink to={"/"}>  My account </NavLink></li>
-              <li><NavLink to={"/"}> Cart </NavLink></li>
-              <li> <NavLink to={"/"}> Wishlist  </NavLink></li>
-              <li> <NavLink to={"/"}> Product Compare </NavLink></li>
+              <li>
+                <NavLink to={"/"}> My account </NavLink>
+              </li>
+              <li>
+                <NavLink to={"/"}> Cart </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Wishlist </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Product Compare </NavLink>
+              </li>
             </ul>
-
           </div>
-
         </div>
+        <div className="footer-down">
+          <p className="footer-txt">© Minimog 2022</p>
+
+          <div className="icons">
+            <a href="https://www.pinterest.com/" target={"_blank"}>
+              {" "}
+              <FaPinterestP className="social-media" />{" "}
+            </a>
+            <a href="https://www.facebook.com/" target={"_blank"}>
+              <FaFacebookF className="social-media" />
+            </a>
+            <a href="https://www.instagram.com/" target={"_blank"}>
+              <FaInstagram className="social-media" />
+            </a>
+            <a href="https://twitter.com/" target={"_blank"}>
+              <FaTwitter className="social-media" />
+            </a>
+              <img src={visaCard} alt="" />
+              <img src={masterCard} alt="" width={"35px"} height={"30px"} />
+              <img src={americanExp} alt="" />
+              <img src={paypal} alt="" />
+              <img src={discover} alt="" />
+            </div>
+          </div>
       </div>
     </div>
   );
