ebc8ab4ffee56092cd3e15950d4d2c8b67c51f99
KhidirovaSveta <khidirova.sveta@gmail.com>
2023-02-13 11:28:18 +0400

    added register page, updated log in page

--- client/src/pages/register/index.jsx
+++ client/src/pages/register/index.jsx
@@ -12,26 +12,32 @@ const Register = () => {
           <div className="navlink">
             <Link to={"/"}> Home </Link>
             <FiChevronRight className="right" />
-            <p>Account</p>
+            <p>Create Account</p>
           </div>
 
-            <div className="register">
-
-          <h3>Register</h3>
-          <div className="registInputs">
-          <input type="text" placeholder="First Name"/>
-          </div>
-          <div className="registInputs">
-          <input type="text" placeholder="Last Name"/>
-          </div>
-          <div className="registInputs">
-          <input type="email" placeholder="Email"/>
-          </div>
-          <div className="registInputs">
-          <input type="password" placeholder="Password"/>
-          </div>
+          <div className="register">
+            <h3 className="registrHeader">Register</h3>
+            <div className="registInputs">
+              <input type="text" placeholder="First Name" />
+            </div>
+            <div className="registInputs">
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <div className="registInputs">
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="registInputs">
+              <input type="password" placeholder="Password" className="borderPink"/>
             </div>
 
+            <p>
+              Sign up for early Sale access plus tailored new arrivals, trends
+              and promotions. To opt out, click unsubscribe in our emails.
+            </p>
+
+            <button className="regBtn btn">REGISTER</button><br />
+            <NavLink to={"/login"}> <button className="loginBtn btn"> LOG IN </button></NavLink>
+          </div>
         </div>
       </div>
     </div>
