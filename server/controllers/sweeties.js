14ea708598fee89a0ab3777d75973e58d505f7f9
KhidirovaSveta <khidirova.sveta@gmail.com>
2023-02-16 21:59:59 +0400

    delete by id

--- server/controllers/sweeties.js
+++ server/controllers/sweeties.js
@@ -1,11 +1,36 @@
-import Sweeties from "../models/index.js"
+import Sweeties from "../models/index.js";
 
-//GET ALL BLOGS
+//get all products
+
 export const getSweeties = async (req, res) => {
-    try {
-      const sweeties = await Sweeties.find();
-      res.status(200).json(sweeties);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+  try {
+    const sweeties = await Sweeties.find();
+    res.status(200).json(sweeties);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//get products by id
+export const getSweetiesById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const sweeties = await Sweeties.findById(id);
+    res.status(200).json(sweeties);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+//delete product by id
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedProduct = await Sweeties.findByIdAndDelete(id);
+    res.json(deletedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/sweeties.js
+++ server/routes/sweeties.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 import { getSweeties } from "../controllers/sweeties.js";
-
+import {getSweetiesById} from "../controllers/sweeties.js"
 const router = express.Router();
 
 router.get("/", getSweeties);
+router.get("/:id", getSweetiesById);
 
 export default router;
